88562dbd4a6c1cd38516fcc2159169e5e8dc5c6a
Oleg <oleg.pak1996@gmail.com>
2023-12-22 23:27:38 +0300

    +

--- lib/features/auth/viev/auth_screen.dart
+++ lib/features/auth/viev/auth_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class AuthWidget extends StatelessWidget {
+  const AuthWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
