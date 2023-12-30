833fed3cf7573318166032445971d8d22883a297
Oleg <oleg.pak1996@gmail.com>
2023-12-30 10:05:08 +0300

    +

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'features/auth/viev/auth_screen.dart';
+import 'package:my_workout/features/welcome/view/welcome_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const AuthWidget(),
+      home: const WelcomeWidget(),
     );
   }
 }
