9fd5671fa649b6a786ca5c1e79e28a68fa4b0141
Oleg <oleg.pak1996@gmail.com>
2024-01-05 12:22:59 +0300

    +

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const WelcomeWidget(),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WelcomeWidget(),
     );
   }
 }
