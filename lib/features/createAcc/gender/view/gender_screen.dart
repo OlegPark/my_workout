0b3abffb902d6b0c2107a91804f777c4c5406fce
Oleg <oleg.pak1996@gmail.com>
2024-01-19 21:24:44 +0300

    +

---
+++ lib/features/createAcc/gender/view/gender_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class genderScreen extends StatelessWidget {
+  const genderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        top: false,
+        bottom: false,
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/gender.png'),
+              fit: BoxFit.cover,
+            )
+          ),
+          child: Column(
+            
+          ),
+        ),
+      ),
+    );
+  }
+}
