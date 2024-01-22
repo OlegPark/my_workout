b500b9fe70fa75b71f41da922121dcfaef645bc2
Oleg <oleg.pak1996@gmail.com>
2024-01-22 21:36:04 +0300

    +

--- lib/features/createAcc/gender/widgets/gender_mid.dart
+++ lib/features/createAcc/gender/widgets/gender_mid.dart
@@ -5,8 +5,16 @@ class MidPanelG extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Image.asset('assets/images/man.png'),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Container(
+          child: Image.asset('assets/images/man.png'),
+        ),
+        Container(
+          child: Image.asset('assets/images/woman.png'),
+        ),
+      ],
     );
   }
 }
