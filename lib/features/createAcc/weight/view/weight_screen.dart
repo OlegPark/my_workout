227486fee11494cf2d820943ed2839b3c11c5905
Oleg <oleg.pak1996@gmail.com>
2024-01-28 21:43:39 +0300

    +

---
+++ lib/features/createAcc/weight/view/weight_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class WeightScreen extends StatelessWidget {
+  const WeightScreen({super.key});
+
+ @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        top: false,
+        bottom: false,
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/height.png'),
+              fit: BoxFit.cover,
+            )
+          ),
+          child: const Column(
+            children: [
+              // TopPanelH(),
+              // MidPanelH(),
+              // BotPanelH(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
