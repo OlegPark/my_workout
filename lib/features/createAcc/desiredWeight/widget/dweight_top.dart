552d3209f500b459ce6db95e104bd3a12c264065
Oleg <oleg.pak1996@gmail.com>
2024-02-01 23:31:25 +0300

    +

--- lib/features/createAcc/desiredWeight/widget/dweight_top.dart
+++ lib/features/createAcc/desiredWeight/widget/dweight_top.dart
@@ -6,7 +6,7 @@ class TopPanelDW extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 30, left: 17),
+      padding: const EdgeInsets.only(top: 50, left: 17),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
