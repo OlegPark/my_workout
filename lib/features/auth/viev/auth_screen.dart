b6c44f87763a7c9ef7a29cd3877ff82093c70743
Oleg <oleg.pak1996@gmail.com>
2024-02-02 14:30:18 +0300

    +

--- lib/features/auth/viev/auth_screen.dart
+++ lib/features/auth/viev/auth_screen.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
 
-class AuthWidget extends StatelessWidget {
-  const AuthWidget({super.key});
+class AuthScreen extends StatelessWidget {
+  const AuthScreen({super.key});
 
-  @override
+   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: SafeArea(
+        top: false,
+        bottom: false,
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/auth.png'),
+              fit: BoxFit.cover,
+            )
+          ),
+          child: const Column(
+            children: [
+              // TopPanelG(),
+              // MidPanelG(),
+              // BotPanelG(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
