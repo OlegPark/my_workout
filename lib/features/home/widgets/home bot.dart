f2d7e1f0a20da62066f9807f550438175ce1c3a3
Oleg <oleg.pak1996@gmail.com>
2024-02-21 17:01:44 +0300

    +

--- lib/features/home/widgets/home bot.dart
+++ lib/features/home/widgets/home bot.dart
@@ -6,5 +6,5 @@ class BotPanelHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
-  }
+  }                                           
 }
