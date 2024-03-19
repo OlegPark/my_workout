b2923264d1d733b3b36765e48776e8442d5576be
Oleg <oleg.pak1996@gmail.com>
2024-03-19 21:51:27 +0300

    +

---
+++ lib/presentation/features/profile/widgets/prof_bot.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BotPanelProf extends StatelessWidget {
+  const BotPanelProf({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
