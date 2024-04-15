b39967a4ba155df8fd338c688afc826295bd6e31
Олег Пак <olegpark@Mac-mini-Oleg.local>
2024-04-15 14:40:22 +0300

    +

---
+++ lib/presentation/features/createW/child_screen/createW1/view/createW1_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CreateW1Screen extends StatelessWidget {
+  const CreateW1Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
