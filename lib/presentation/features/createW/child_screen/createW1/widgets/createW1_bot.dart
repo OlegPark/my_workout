0220cdd2c85e252540f2090365869c2274720f26
Олег Пак <olegpark@Mac-mini-Oleg.local>
2024-04-19 21:50:55 +0300

    +

---
+++ lib/presentation/features/createW/child_screen/createW1/widgets/createW1_bot.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CrWBot extends StatelessWidget {
+  const CrWBot({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/presentation/features/createW/child_screen/createW1/widgets/createW1_mid.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+Widget CrWMid() {
+  return SingleChildScrollView(
+    child: Column(
+      children: [
+        
+      ],
+    ),
+  );
+}
