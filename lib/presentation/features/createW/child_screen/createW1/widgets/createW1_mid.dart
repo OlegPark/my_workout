a93e1d8dd17a56adeaadaaa1c27bf47dfa428bd5
Олег Пак <olegpark@Mac-mini-Oleg.local>
2024-04-20 15:47:17 +0300

    +

--- lib/presentation/features/createW/child_screen/createW1/widgets/createW1_mid.dart
+++ lib/presentation/features/createW/child_screen/createW1/widgets/createW1_mid.dart
@@ -1,11 +1,33 @@
 import 'package:flutter/material.dart';
 
-Widget CrWMid() {
-  return SingleChildScrollView(
-    child: Column(
-      children: [
-        
-      ],
-    ),
-  );
+class CrWMid extends StatelessWidget {
+  const CrWMid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          GestureDetector(
+            onTap: () {
+              
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[300],
+                borderRadius: BorderRadius.circular(10),
+              ),
+              height: 30,
+              width: 200,
+              child: const Center(
+                child: Text(
+                  '+ Добавить упражнение',
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
