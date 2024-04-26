fe84cc29eb678fbca62202af2b3a24f50294a14b
Олег Пак <oleg.pak1996@gmail.com>
2024-04-26 15:22:27 +0300

    +

--- lib/data/workout_data.dart
+++ lib/data/workout_data.dart
@@ -24,7 +24,25 @@ class WorkoutData {
   }
 
   // добавить тренировку
+  void addWorkout(String name) {
+    // dobavili novuy trinirovku v obshiy spisok trenirovok
+    workoutList.add(Workout(name: name, exercise: []));
+  }
+
   // добавить упржнение
+  void addExercise(String workoutName, String exerciseName, String weight, String reps, String sets) {
+    // find the relevant workout
+    Workout relevantWorkout = workoutList.firstWhere((workout) => workout.name == workoutName);
+    relevantWorkout.exercise.add(
+      Exercise(
+        name: exerciseName,
+        weight: weight,
+        reps: reps,
+        sets: sets
+      ),
+    );
+  }
+
   // отметить упражнение после его завершения
   // продолжительность тренировки
 }
