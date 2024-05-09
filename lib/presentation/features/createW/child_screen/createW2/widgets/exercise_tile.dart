6ede84a47a84d0cad028010813aef425459dcb4c
Олег Пак <oleg.pak1996@gmail.com>
2024-05-09 14:24:59 +0300

    +

---
+++ lib/presentation/features/createW/child_screen/createW2/widgets/exercise_tile.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class ExerciseTile extends StatelessWidget {
+  final String exerciseName;
+  final String weight;
+  final String reps;
+  final String sets;
+  final bool isCompleted;
+
+  const ExerciseTile({
+    super.key,
+    required this.exerciseName,
+    required this.weight,
+    required this.reps,
+    required this.sets,
+    required this.isCompleted,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey,
+      child: ListTile(
+        title: Text(exerciseName),
+        subtitle: Row(
+          children: [
+            //ves
+            Chip(label: Text("${weight}kg")),
+            
+            //povtoreniya
+            Chip(label: Text("$reps reps")),
+            
+            //podhod
+            Chip(label: Text("$sets sets")),
+          ],
+        ),
+      ),
+    );
+  }
+}
