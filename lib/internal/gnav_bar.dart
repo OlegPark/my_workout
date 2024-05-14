61ea3c33838827a290982b9f7f9b3346e1e1da4c
Олег Пак <oleg.pak1996@gmail.com>
2024-05-14 15:24:25 +0300

    +

---
+++ lib/internal/gnav_bar.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class GoogleNavBar extends StatelessWidget {
+  const GoogleNavBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text("NAV BAR"),),
+    );
+  }
+}
