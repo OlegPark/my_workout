e72e922cae10c186d07df7e421ba2aeae2374e72
Олег Пак <oleg.pak1996@gmail.com>
2024-05-25 14:24:19 +0300

    +

--- lib/presentation/features/profile/widgets/heat_map.dart
+++ lib/presentation/features/profile/widgets/heat_map.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_heatmap_calendar/flutter_heatmap_calendar.dart';
+import 'package:my_workout/domain/datetime/date_time.dart';
 
 class MyHeatMap extends StatelessWidget {
   final Map<DateTime, int>? datasets;
@@ -9,6 +11,23 @@ class MyHeatMap extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: EdgeInsets.all(25),
+      child: HeatMap(
+        startDate: createDateTimeObject(startDateYYYYMMDD),
+        endDate: DateTime.now().add(const Duration(days: 0)),
+        datasets: datasets,
+        colorMode: ColorMode.color,
+        defaultColor: Colors.grey[200],
+        textColor: Colors.white,
+        showColorTip: false,
+        showText: true,
+        scrollable: true,
+        size: 30,
+        colorsets: const {
+          1: Colors.green,
+        },
+      ),
+    );
   }
 }
