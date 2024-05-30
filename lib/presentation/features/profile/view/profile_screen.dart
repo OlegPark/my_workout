79c0a2d28fb384835e97f7499f16da6dbf530c76
Олег Пак <oleg.pak1996@gmail.com>
2024-05-30 15:10:31 +0300

    +

--- lib/presentation/features/profile/view/profile_screen.dart
+++ lib/presentation/features/profile/view/profile_screen.dart
@@ -18,18 +18,50 @@ class _ProfileScreenState extends State<ProfileScreen> {
       builder: (context, value, child) => Scaffold(
         appBar: AppBar(
           backgroundColor: backgroundDark,
-          title: Text(
+          title: const Text(
             'Мой прогресс',
             style: TextStyle(
               color: Colors.white
             ),
           ),
+          centerTitle: false,
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.settings),
+              color: Colors.white,
+            ),
+          ],
         ),
         backgroundColor: backgroundDark,
         body: ListView(
           children: [
+            const Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: Row(
+                children: [
+                  Icon(Icons.circle, color: Colors.grey, size: 90,),
+                  SizedBox(width: 10,),
+                  Padding(
+                    padding: EdgeInsets.only(top: 20),
+                    child: Text('Олег Пак', style: TextStyle(color: Colors.white, fontSize: 25),),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20,),
+            const Center(child: Text('График тренировок', style: TextStyle(color: Colors.white, fontSize: 18),)),
             MyHeatMap(datasets: value.heatMapDataSet, startDateYYYYMMDD: value.getStartDate()),
-            Center(child: Text('Profile screeen', style: TextStyle(color: Colors.white),)),
+            const SizedBox(height: 20,),
+            const Center(child: Text('Показатели', style: TextStyle(color: Colors.white, fontSize: 18),)),
+            const SizedBox(height: 20,),
+            Container(
+              height: 300,
+              width: 300,
+              decoration: BoxDecoration(
+                image: DecorationImage(image: AssetImage('assets/images/bodymap.jpg'))
+              ),
+            )
           ],
         ),
       ),
