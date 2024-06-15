a0894f6902bccdd52f66a5ae397c4b5140a3fd9d
Олег Пак <oleg.pak1996@gmail.com>
2024-06-15 14:39:37 +0300

    +

--- lib/presentation/features/createAcc/height/view/height_screen.dart
+++ lib/presentation/features/createAcc/height/view/height_screen.dart
@@ -80,26 +80,3 @@ class _HeightScreenState extends State<HeightScreen> {
     );
   }
 }
-
-
-// body: SafeArea(
-//         top: false,
-//         bottom: false,
-//         child: Container(
-//           width: double.infinity,
-//           height: double.infinity,
-//           decoration: const BoxDecoration(
-//             image: DecorationImage(
-//               image: AssetImage('assets/images/height.png'),
-//               fit: BoxFit.cover,
-//             )
-//           ),
-//           child: const Column(
-//             children: [
-//               TopPanelH(),
-//               MidPanelH(),
-//               BotPanelH(),
-//             ],
-//           ),
-//         ),
-//       ),
